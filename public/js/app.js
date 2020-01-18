8bbc27897a6bc23f8f07a5778c15a900a7043b1c
Kofi Bamfo Quaicoe <33463657+bamfokbq@users.noreply.github.com>
2020-01-18 20:07:43 +0100

    Connected the interface

---
+++ public/js/app.js
@@ -0,0 +1,18 @@
+const weatherform = document.querySelector('form');
+const search = document.querySelector('input');
+
+weatherform.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const location = search.value;
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                console.log(data.forecast);
+                console.log(data.location);
+            }
+        })
+    })
+})
