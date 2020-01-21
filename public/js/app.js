d4075751c43aff11319b418b805aab415ff2ddaa
Kofi Bamfo Quaicoe <33463657+bamfokbq@users.noreply.github.com>
2020-01-21 23:36:41 +0100

    Added new files to the project

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,27 @@
 const weatherform = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#message__one')
+const messageTwo = document.querySelector('#message__two')
+
 
 weatherform.addEventListener('submit', (e) => {
     e.preventDefault();
     const location = search.value;
-
+    // Default input
+    messageOne.textContent = "Loading......";
+    messageTwo.textContent = " ";
+    // Fetching from the server
     fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
             if (data.error) {
-                console.log(data.error);
+                messageOne.textContent = data.error;
             } else {
-                console.log(data.forecast);
-                console.log(data.location);
+                messageOne.textContent = data.location;
+                messageTwo.textContent = data.forecast;
             }
         })
+        messageOne.textContent = ""
+        messageTwo.textContent = ""
+
     })
 })
